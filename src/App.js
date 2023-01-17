4278bf50f94310959391cbccd98296cfcb3adf90
dheekkshaR <116768851+dheekkshaR@users.noreply.github.com>
2023-01-16 23:30:09 -0500

    paragraph commit2

--- src/App.js
+++ src/App.js
@@ -7,13 +7,13 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HIIII anvithaaa,<br>
-          My favourite akka, Have a happy happy happy Birthday!!!<br>
-          You are the nicest kindest person Iv met and im so lucky to have you as a roommate</br>
-          Have a blast on ur bday! </br>
-          I wish you all the succeess and blessings for everything this upcoming year and Il be there to see all of it.<br>
-          HAPPY BIRTHDAY BROOO</br>
-            
+          HIIII anvithaaa,<br/>
+          My favourite akka, Have a happy happy happy Birthday!!!<br/>
+          You are the nicest kindest person Iv met and im so lucky to have you as a roommate<br/>
+          Have a blast on ur bday! <br/>
+          I wish you all the succeess and blessings for everything this upcoming year and Il be there to see all of it.<br/>
+          HAPPY BIRTHDAY BROOO
+
         </p>
 
       </header>
