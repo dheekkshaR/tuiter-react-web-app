b5ab77dfd8885eb509f20cfc73e9c29922867bd8
dheekkshaR <116768851+dheekkshaR@users.noreply.github.com>
2023-01-16 23:27:36 -0500

    paragraph commit2

--- src/App.js
+++ src/App.js
@@ -7,23 +7,15 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          HIIII anvithaaa,
-          My favourite akka, Have a happy happy happy Birthday!!!
-          You are the nicest kindest person Iv met and im so lucky to have you as a roommate
-          Have a blast on ur bday! I wish you all the succeess and blessings for everything this upcoming year and Il be there to see all of it.
-          HAPPY BIRTHDAY BROOO
-
-
-
+          HIIII anvithaaa,<br>
+          My favourite akka, Have a happy happy happy Birthday!!!<br>
+          You are the nicest kindest person Iv met and im so lucky to have you as a roommate</br>
+          Have a blast on ur bday! </br>
+          I wish you all the succeess and blessings for everything this upcoming year and Il be there to see all of it.<br>
+          HAPPY BIRTHDAY BROOO</br>
+            
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+
       </header>
     </div>
   );
